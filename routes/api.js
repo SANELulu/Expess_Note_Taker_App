60f03ef3819084a2b0e13bdf113a2c1ec7f0eb40
Lucas Diaz <lucasdiaz22b@gmail.com>
2021-03-10 00:24:25 -0500

    added uuid, fixed delete request. app functioning as designed

--- routes/api.js
+++ routes/api.js
@@ -1,92 +1,34 @@
 const fs = require('fs');
 const path = require('path');
-const data = require('../db/db.json')
-
+const data = require('../db/db.json');
+const { v4: uuidv4 } = require('uuid');
 
 module.exports = function (app) {
+  //GET for all notes in .json file
     app.get('/api/notes', function (req,res){
+      res.sendFile(path.join(__dirname, '../db/db.json'))
+    })
+
+//POST for new notes into .json file 
+    app.post('/api/notes', (req,res) => {
+      let newNote = req.body;
+      newNote.id = uuidv4();
+      data.push(newNote) 
+      fs.writeFileSync('./db/db.json', JSON.stringify(data))
       res.json(data)
-        // fs.readFile(path.join(__dirname, '../db/db.json'),'utf8',(err, data) => {
-          
-        //     if (err) {
-        //       console.error(err)
-        //       return
-        //     }
-        //     data = JSON.parse(data)
-        //     console.log(data)
-        //     // console.log(reg.body)
-        //     res.json(data)
-        //   })
+      console.log("added new note!")
     })
-    app.post('/api/notes', function (req,res){
-        // let savedNote = req.body;
-        // let id = '' + postNote;
-        // savedNote.id = id;
-        // postNote = postNote + 1;
-        // data.push(savedNote);
-        fs.writeFile('../db/db.json', JSON.stringify(data), () => {
-            console.log(data)
-            console.log("done")
-            res.json(data);
-        });
-        // res.json(savedNote);
+
+  //DELETE for selected note from .json file
+    app.delete('/api/notes/:id', function (req, res) {
+              let id = req.params.id
+              const select = data.find(note => note.id === id)
+
+              if (id === select.id){
+                  data.splice(data.indexOf(select),1) 
+                  fs.writeFileSync('./db/db.json', JSON.stringify(data))
+              }
+              res.json(data)
+              console.log("deleted note!")
     })
-//     app.post('/api/notes', function (req, res) {
-//       let savedNote = req.body;
-//       let id = '' + postNote;
-//       savedNote.id = id;
-//       postNote = postNote + 1;
-//       data.push(savedNote);
-//       fs.writeFile('../db/db.json', JSON.stringify(data), () => {
-//           console.log('note successful')
-//       });
-//       res.json(savedNote);
-//   }),
-//   app.delete('/api/notes/:id', function (req, res) {
-//     let selected = req.params.id;
-//     for (let index = 0; index < data.length; index++) {
-//         if (selected === data[index].id) {
-//             data.splice(index, 1);
-//             fs.writeFile('../db/db.json', JSON.stringify(data), () => {
-//                 console.log('deleted!')
-//             });
-//         };
-//     };
-//     res.json(data)
-// })
 }
-
-
-// // const fs = require('fs');
-// // const data = require('../db/db.json')
-// // module.exports = function (app) {
-// //     let postNote = data.length;
-// //     app.get('/api/notes', function (req, res) {
-// //         res.json(data)
-// //     });
-// //     // function adds and saves notes to server and logs 'note succesfull'
-//     app.post('/api/notes', function (req, res) {
-//         let savedNote = req.body;
-//         let id = '' + postNote;
-//         savedNote.id = id;
-//         postNote = postNote + 1;
-//         data.push(savedNote);
-//         fs.writeFile('../db/db.json', JSON.stringify(data), () => {
-//             console.log('note successful')
-//         });
-//         res.json(savedNote);
-//     });
-// //     // function to delete notes
-// //     app.delete('/api/notes/:id', function (req, res) {
-// //         let selected = req.params.id;
-// //         for (let index = 0; index < data.length; index++) {
-// //             if (selected === data[index].id) {
-// //                 data.splice(index, 1);
-// //                 fs.writeFile('../db/db.json', JSON.stringify(data), () => {
-// //                     console.log('deleted!')
-// //                 });
-// //             };
-// //         };
-// //         res.json(data)
-// //     })
-// // }
--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const app = express();
-const router = express.Router();
 
-//
 const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-//routes
 require('./routes/api')(app);
 require('./routes/html')(app);
 
